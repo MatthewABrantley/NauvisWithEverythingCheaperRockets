88c3a0f80dbfa45b256d4a16ad18cd6d98894ab6
alchav <alchav@jalchavware.com>
2024-12-04 02:57:26 -0500

    Fix desyncs?

--- control.lua
+++ control.lua
@@ -1,21 +1,32 @@
 local handler = require("event_handler")
-
--- Force the use of silo-script regardless of Space Age mod
 handler.add_lib(require("silo-script"))
-script.on_init(function()
-    local info =
+
+local init_ending_info = function()
+  local info =
+  {
+    image_path = "victory.png",
+    title = {"gui-game-finished.victory"},
+    message = {"victory-message"},
+    bullet_points =
     {
-        image_path = is_space_age and "victory-space-age.png" or "victory.png",
-        title = {"gui-game-finished.victory"},
-        message = {"victory-message"},
-        bullet_points =
-        {
-            {"victory-bullet-point-1"},
-            {"victory-bullet-point-2"},
-            {"victory-bullet-point-3"},
-            {"victory-bullet-point-4"}
-        },
-        final_message = {"victory-final-message"},
-    }
-    game.set_win_ending_info(info)
-end)
+      {"victory-bullet-point-1"},
+      {"victory-bullet-point-2"},
+      {"victory-bullet-point-3"},
+      {"victory-bullet-point-4"}
+    },
+    final_message = {"victory-final-message"},
+  }
+  game.set_win_ending_info(info)
+end
+
+local re_add_silo_script = {}
+
+re_add_silo_script.on_configuration_changed = function()
+  init_ending_info()
+end
+
+re_add_silo_script.on_init = function()
+  init_ending_info()
+end
+
+handler.add_lib(re_add_silo_script)
